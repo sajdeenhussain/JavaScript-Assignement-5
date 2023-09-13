cf8e55c3a943d23a21acc2109ae092741b268f44
sajdeenhussain <sajideen7755@gmail.com>
2023-09-13 22:37:35 +0500

    assignment-5

---
+++ app.js
@@ -0,0 +1,40 @@
+// Write a program that takes a positive integer from user &
+//display the following in your browser.
+//a. number
+//b. round off value of the number
+//c. floor value of the number
+//d. ceil value of the number
+
+// var userInput = prompt("enter your number");
+// var input = userInput;
+// console.log(input);
+// console.log(Math.round(input));
+// console.log(Math.floor(input));
+// console.log(Math.ceil(input));
+
+
+
+
+// Write a program that takes a negative floating point
+// number from user & display the following in your browser.
+// a. number
+// b. round off value of the number
+// c. floor value of the number
+// d. ceil value of the number
+// var userInput = prompt("enter your number");
+// var input = userInput;
+// console.log(input);
+// console.log(Math.round(input));
+// console.log(Math.floor(input));
+// console.log(Math.ceil(input));
+
+// Write a program that shows a random number between 1
+// and 100 in your browser.
+// var randomNumber = Math.ceil(Math.random() * 100);
+// console.log(randomNumber)
+
+
+// Write a program that displays current date and time in
+// your browser.
+// var date = new Date();
+// console.log(date)
